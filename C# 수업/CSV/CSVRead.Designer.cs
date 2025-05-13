e358b1626898853042b92a2fda15374ecec54ae3
jacksimuse <cjh930306@naver.com>
2025-05-13 09:04:22 +0900

    Add files via upload

---
+++ C# 수업/CSV/CSVRead.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CSV
+{
+    partial class CSVRead
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CSVRead));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 350);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btn_send);
+            this.panel2.Controls.Add(this.btn_refresh);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 350);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_refresh.Image = ((System.Drawing.Image)(resources.GetObject("btn_refresh.Image")));
+            this.btn_refresh.Location = new System.Drawing.Point(12, 6);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(152, 82);
+            this.btn_refresh.TabIndex = 0;
+            this.btn_refresh.Text = "새로고침";
+            this.btn_refresh.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_send.Image = ((System.Drawing.Image)(resources.GetObject("btn_send.Image")));
+            this.btn_send.Location = new System.Drawing.Point(637, 6);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(151, 82);
+            this.btn_send.TabIndex = 1;
+            this.btn_send.Text = "전송";
+            this.btn_send.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_send.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 350);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // CSVRead
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "CSVRead";
+            this.Text = "CSVRead";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Button btn_refresh;
+        public System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ C# 수업/CSV/CSVWrite.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV
+{
+    public partial class CSVWrite: Form
+    {
+        public CSVWrite()
+        {
+            InitializeComponent();
+        }
+
+        #region 함수
+
+        private void CreateCsv()
+        {
+            StringBuilder sb = new StringBuilder(); // DataGridView에 있는 모든 데이터를 CSV파일로 만들기 위해 문자열로 담아줄 그릇
+
+            // 헤더
+            for (int i = 0; i < dgv_getdata.Columns.Count; i++) 
+            {
+                sb.Append(dgv_getdata.Columns[i].HeaderText); // 각 열에 있는 제목을 sb에 담음
+                if (i <  dgv_getdata.Columns.Count - 1)
+                    sb.Append(", ");
+            }
+            sb.AppendLine();
+
+            // 데이터 추가
+            foreach (DataGridViewRow row in dgv_getdata.Rows)
+            {
+                if (!row.IsNewRow) // 마지막 줄이 아니라면
+                {
+                    for (int i = 0; i < dgv_getdata.Columns.Count; i++)
+                    {
+                        sb.Append(row.Cells[i].Value); // 각 칸에 있는 값을 sb에 담음
+                        if (i <dgv_getdata.Columns.Count-1)
+                            sb.Append(", ");
+                    }
+                    sb.AppendLine();
+                }
+            }
+
+            // CSV 파일 생성
+            string filePath = "data.csv";
+            if (File.Exists(filePath) == false) // csv 파일 존재 유무 확인, 없으면 생성
+            {
+                File.WriteAllText(filePath, sb.ToString(), Encoding.UTF8);
+            }
+
+            if (IsFileLocked(filePath)) 
+            {
+                timer1.Stop();
+                // MessageBox.Show("내용", "제목", MessageBoxButtons, MessageBoxIcon);
+                DialogResult = MessageBox.Show("파일이 실행중입니다. 창을 닫아주세요", "창 닫기", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                if (DialogResult == DialogResult.OK) // 창을 닫고 확인했을 때
+                {
+                    CreateCsv();
+                }
+            }
+            else
+            {
+                timer1.Start();
+                File.WriteAllText(filePath, sb.ToString(), Encoding.UTF8);
+            }
+        }
+
+        private bool IsFileLocked(string filePath)
+        {
+            try
+            {
+                // 파일을 읽고 쓰기 모드로 열고, 공유 안됨 
+                using (FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                {
+                    return false; // 파일을 열 수 있음
+                }
+            }
+            catch (IOException)
+            {
+                return true; // 파일이 열려 있어서 잠김
+            }
+        }
+
+        private void CreateData()
+        {
+            int rowIndex = dgv_getdata.Rows.Add(); // datagridview에서 새롭게 한줄 추가
+            DataGridViewRow newRow = dgv_getdata.Rows[rowIndex]; // datagridview에서 한 행이 몇번째 행인지 대입을 통해 지정해줌
+
+            for (int i = 0; i < dgv_getdata.Columns.Count; i++) // 만들어진 열을 돌면서 데이터를 채움
+            {
+                // 10 01 01 01  readdata[3] 
+                // if (readdata[3]  == 1) {"전진"}
+
+                newRow.Cells[i].Value = (rowIndex + 1) * (i + 1); //한 줄 안의 한 칸을 값으로 채움
+            }
+        }
+
+        private void ShowData()
+        {
+            string filePath = "data.csv"; // 읽어올 파일
+            DataTable csvData = ReadCsvFile(filePath); // 파일을 읽어서 데이터 테이블에 입력
+
+            // CSVRead 화면 띄우기
+            CSVRead csvform = new CSVRead();
+            csvform.dataGridView1.DataSource = csvData; // 다른 폼에 있는 DataGridView에 접근하여 데이터 테이블을 할당
+            csvform.Show(); // 화면 띄우기
+
+        }
+
+        private DataTable ReadCsvFile(string filePath)
+        {
+            DataTable dt = new DataTable(); // 데이터 테이블 객체 생성
+            try
+            {
+                using(StreamReader sr = new StreamReader(filePath)) // 파일을 읽어옴
+                {
+                    string[] headers = sr.ReadLine().Split(','); // "1번 데이터, 2번 데이터, 3번 데이터" => [ "1번 데이터",  "2번 데이터", "3번 데이터"]
+                    foreach (string line in headers)
+                    {
+                        dt.Columns.Add(line);
+                    }
+                    while(!sr.EndOfStream)
+                    {
+                        string[] rows = sr.ReadLine().Split(',');
+                        DataRow dr = dt.NewRow();
+                        for (int i = 0; i < headers.Length; i++)
+                        {
+                            dr[i] = rows[i];
+                        }
+                        dt.Rows.Add(dr);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"CSV파일 오류 : {ex.Message}");
+            }
+            return dt;
+        }
+
+        #endregion
+
+        #region 이벤트
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CreateData();
+            CreateCsv();
+        }
+
+        private void btn_CSVread_Click(object sender, EventArgs e)
+        {
+            ShowData();
+        }
+
+        #endregion
+
+    }
+}
